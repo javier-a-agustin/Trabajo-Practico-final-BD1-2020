4b5764bd4ecc6ded31e20a012cb89657c795b328
Ramiro <57329496+ramirocanario1@users.noreply.github.com>
2020-12-17 17:43:13 -0300

    Update datos.sql

--- datos.sql
+++ datos.sql
@@ -14,12 +14,20 @@ select * from persona;
 
 
 INSERT INTO fabrica(cuit, calle, numero) VALUES 
-	('11-22211-9', 'Belgrano', 30),
-	('22-22-22', 'Perito Moreno',1590);
+	--('11-22211-9', 'Belgrano', 30),
+	--('22-22-22', 'Perito Moreno',1590);
+	('30-18273849-9', 'Colombia', 458),
+	('30-18293004-9', 'Marconi', 899),
+	('30-66736278-9', 'Pellegrini', 1706),
+	('30-83748929-9', 'Belgrano', 1923);
 
 INSERT INTO producto(nombre, precio, presentacion, cuit_fabrica) VALUES
-('Pileta de Lona 3,50 MT x 2,5 MT', 7000, 'En caja', '11-22211-9'),
-('Maceta 10 LTS', 1000, 'Suelto', '22-22-22');
+--('Pileta de Lona 3,50 MT x 2,5 MT', 7000, 'En caja', '11-22211-9'),
+--('Maceta 10 LTS', 1000, 'Suelto', '22-22-22');
+--('Vasos descartables', 100, 'Pack x20', '30-18273849-9');
+--('Resma A4', 300, 'Paquete x500 hojas', '30-18293004-9');
+--('Taza de ceramica 330ml', 280, 'Suelto', '30-66736278-9');
+('Papel burbuja', 450, 'Rollo 25 mts', '30-83748929-9');
 
 select * from producto;
 
